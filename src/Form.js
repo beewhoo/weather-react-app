ffe06ec239656db288d25456dd7d70681cadcdde
bibek <bibeksinghthapa@gmail.com>
2018-09-06 16:26:23 -0400

    proved

--- src/Form.js
+++ src/Form.js
@@ -9,23 +9,40 @@ class Form extends Component {
     super(props)
     this.state = {
 
-      city1:'',
+
+
       highTemp1:'',
       lowTemp1:'',
       description1:'',
       img1:'https://media.giphy.com/media/26u6dryuZH98z5KuY/giphy.gif',
 
-      city2:'',
+
       highTemp2:'',
       lowTemp2:'',
       description2:'',
       img2:'https://media.giphy.com/media/26u6dryuZH98z5KuY/giphy.gif',
 
-      city3:'',
+
+
       highTemp3:'',
       lowTemp3:'',
       description3:'',
       img3:'https://media.giphy.com/media/26u6dryuZH98z5KuY/giphy.gif',
+
+
+
+      highTemp4:'',
+      lowTemp4:'',
+      description4:'',
+      img4:'https://media.giphy.com/media/26u6dryuZH98z5KuY/giphy.gif',
+
+
+
+      highTemp5:'',
+      lowTemp5:'',
+      description5:'',
+      img5:'https://media.giphy.com/media/26u6dryuZH98z5KuY/giphy.gif',
+
 
 
 
@@ -46,30 +63,41 @@ class Form extends Component {
 
     let base = this
 
-   fetch('https://api.openweathermap.org/data/2.5/forecast/daily?q=' + this.state.city + '&cnt=3&units=metric&appid=052f26926ae9784c2d677ca7bc5dec98')
+   fetch('https://api.openweathermap.org/data/2.5/forecast/daily?q=' + this.state.city + '&cnt=5&units=metric&appid=052f26926ae9784c2d677ca7bc5dec98')
      .then(function(response) {
        return response.json()
      }).then(function(json) {
 
        // update state
        base.setState({
-         city1: json.city.name,
+
          description1: json.list[0].weather[0].description,
-         highTemp1: json.list[0].temp.max + " " + '°C',
-         lowTemp1: json.list[0].temp.min +  " " + '°C',
+         highTemp1: json.list[0].temp.max + ' ' + '°C',
+         lowTemp1: json.list[0].temp.min +  ' ' + '°C',
          img1: 'https://openweathermap.org/img/w/' + json.list[0].weather[0].icon + '.png',
 
 
          description2: json.list[1].weather[0].description,
-         highTemp2: json.list[1].temp.max + " " + '°C',
-         lowTemp2: json.list[1].temp.min +  " " + '°C',
+         highTemp2: json.list[1].temp.max + ' ' + '°C',
+         lowTemp2: json.list[1].temp.min +  ' ' + '°C',
          img2: 'https://openweathermap.org/img/w/' + json.list[1].weather[0].icon + '.png',
 
 
          description3: json.list[2].weather[0].description,
-         highTemp3:  json.list[2].temp.max + " " + '°C',
-         lowTemp3: json.list[2].temp.min +  " " + '°C',
-         img3: 'https://openweathermap.org/img/w/' + json.list[2].weather[0].icon + '.png'
+         highTemp3:  json.list[2].temp.max + ' ' + '°C',
+         lowTemp3: json.list[2].temp.min +  ' ' + '°C',
+         img3: 'https://openweathermap.org/img/w/' + json.list[2].weather[0].icon + '.png',
+
+         description4: json.list[3].weather[0].description,
+         highTemp4:  json.list[3].temp.max + ' ' + '°C',
+         lowTemp4: json.list[3].temp.min +  ' '+ '°C',
+         img4: 'https://openweathermap.org/img/w/' + json.list[3].weather[0].icon + '.png',
+
+         description5: json.list[4].weather[0].description,
+         highTemp5:  json.list[4].temp.max + ' ' + '°C',
+         lowTemp5: json.list[4].temp.min +  ' ' + '°C',
+         img5: 'https://openweathermap.org/img/w/' + json.list[4].weather[0].icon + '.png'
+
 
 
 
@@ -93,7 +121,7 @@ class Form extends Component {
 
 
         <label>
-          <input type="text" onChange={this.handleChange} />
+          <input type="text" placeholder="search by city" onChange={this.handleChange} />
         </label>
 
         <p><input className='button' type="submit" value="Get forecast" /></p>
@@ -103,7 +131,7 @@ class Form extends Component {
             <img src={this.state.img1} alt='icon' width='100px'/>
             <p>{this.state.highTemp1}</p>
             <p>{this.state.lowTemp1}</p>
-            <p>{this.state.description1}</p>
+            <p>{this.state.description1.toUpperCase()}</p>
           </div>
 
 
@@ -111,14 +139,28 @@ class Form extends Component {
             <img src={this.state.img2} alt='icon' width='100px'/>
             <p>{this.state.highTemp2}</p>
             <p>{this.state.lowTemp2}</p>
-            <p>{this.state.description2}</p>
+            <p>{this.state.description2.toUpperCase()}</p>
           </div>
 
           <div className='forecast'>
             <img src={this.state.img3} alt='icon' width='100px'/>
             <p>{this.state.highTemp3}</p>
             <p>{this.state.lowTemp3}</p>
-            <p>{this.state.description3}</p>
+            <p>{this.state.description3.toUpperCase()}</p>
+          </div>
+
+          <div className='forecast'>
+            <img src={this.state.img4} alt='icon' width='100px'/>
+            <p>{this.state.highTemp4}</p>
+            <p>{this.state.lowTemp4}</p>
+            <p>{this.state.description4.toUpperCase()}</p>
+          </div>
+
+          <div className='forecast'>
+            <img src={this.state.img5} alt='icon' width='100px'/>
+            <p>{this.state.highTemp5}</p>
+            <p>{this.state.lowTemp5}</p>
+            <p>{this.state.description5.toUpperCase()}</p>
           </div>
 
 
